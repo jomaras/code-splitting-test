f97c00c7b04c8c80def627205b01ed4ce5de898d
jomaras <jomaras@gmail.com>
2018-09-03 12:58:36 +0200

    loading external components

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -8,5 +8,13 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         const CounterRenderer = (await import(/* webpackChunkName: "counter" */'./counter/Counter')).CounterRenderer;
         CounterRenderer.render(container);
-    });    
+    });
+
+    document.querySelector(".external-component-container > button").addEventListener("click", async () => {
+        const container = document.querySelector(".external-component-container");
+        container.innerHTML = "";
+
+        const EmailValidatorRenderer = (await import(/* webpackChunkName: "email-validator" */'typescript-components-test/src/email-validator/EmailValidator')).EmailValidatorRenderer;
+        EmailValidatorRenderer.render(container);
+    });
 });
